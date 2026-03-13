f402b6e4b5de4475d10e2b3849e354419ec6d110
Genny4Real1 <and.russo.0511@gmail.com>
2026-03-13 17:51:13 +0100

    New: - Updated README - updated logic to add subplots, with the objective of adding widgets in the future - small scope bug fixed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Equilibrium-Simulator
+
+## Objective
+* Simulate mechanical equilibrium of an object on a plane,
+* Exactly draw the force vectors applied to the object,
+* Make every information accessible to the user, so he can simulate different conditions by changing every values, from the environment to the object etc...
+
+## Future
+* Make it a real time simulation. The user should be able to input data and the graph should update immediately with the new data.
--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 """ Main script"""
 import math
 import matplotlib.pyplot as plt
+# from matplotlib.widgets import CheckButtons
 
 # Constants
 GRAVITY = 9.81 # m/s^2
 
 # Test Data
 mass = 10 #kg
-theta_degrees = 40
+theta_degrees = 0
 theta_rad = math.radians(theta_degrees)
+
+# Matplotlib
+fig, ax = plt.subplots(figsize=(10,6))
 
 # Labels
 labels = {'W' : "Weight", 'C' : "Constraint"}
@@ -18,7 +22,7 @@ def calculate_coords(l):
     y = l * math.sin(theta_rad)
     return x, y
 
-def calculate_weight(mass):
+def calculate_weight():
     f_weight = mass * GRAVITY #N = kg*m/s^2
     f_parallel = f_weight * math.sin(theta_rad) #N = kg*m/s^2
     f_perpendicular = f_weight * math.cos(theta_rad) #N = kg*m/s^2
@@ -29,24 +33,25 @@ def main():
     l = 100
     x, y = calculate_coords(l)
     x0, y0 = x/2, y/2
-    f_weight, f_parallel, f_perpendicular = calculate_weight(mass)
+    f_weight, f_parallel, f_perpendicular = calculate_weight()
     draw_simulation(x, y, x0, y0, f_weight, f_perpendicular)
 
 def draw_simulation(x, y, x0, y0, f_weight, f_perpendicular):
-    plt.figure(figsize=(10, 6))
-    plt.plot([0, x], [0, 0], 'gray', linestyle='--')  # Base
-    plt.plot([x, x], [0, y], 'gray', linestyle='--')  # Altezza
-    plt.plot([0, x], [0, y], 'black', linewidth=3)  # Piano effettivo
-    plt.quiver(x0, y0, 0, -f_weight, color='red', label=labels['W'], angles='xy', scale_units='xy', scale=10)
+    ax.plot([0, x], [0, 0], 'gray', linestyle='--')  # Base
+    ax.plot([x, x], [0, y], 'gray', linestyle='--')  # Altezza
+    ax.plot([0, x], [0, y], 'black', linewidth=3)  # Piano effettivo
+    ax.quiver(x0, y0, 0, -f_weight, color='red', label=labels['W'], angles='xy', scale_units='xy', scale=10)
     # x & y are the coordinates of the origin point, u & v are the components of the vector
     un = -f_perpendicular * math.sin(theta_rad)
     vn = f_perpendicular * math.cos(theta_rad)
-    plt.quiver(x0, y0, un, vn, color='blue', label=labels['C'], angles='xy', scale_units='xy', scale=10)
+    ax.quiver(x0, y0, un, vn, color='blue', label=labels['C'], angles='xy', scale_units='xy', scale=10)
 
-    plt.axis('equal')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    ax.axis('equal')
+    ax.legend()
+    ax.grid(True)
+    fig.subplots_adjust(left=0.3)
+    ax_buttons = fig.add_axes([0.05, 0.4, 0.15, 0.25])
+    fig.show()
 
 
 
