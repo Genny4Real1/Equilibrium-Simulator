caa83372fcb7eeec7add65eb604bbd7708c75577
Genny4Real1 <and.russo.0511@gmail.com>
2026-03-18 17:59:41 +0100

    New: - Toggle vector logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """ Main script"""
 import math
 import matplotlib.pyplot as plt
-# from matplotlib.widgets import CheckButtons
+from matplotlib.widgets import CheckButtons
 
 # Constants
 GRAVITY = 9.81 # m/s^2
@@ -10,12 +10,10 @@ GRAVITY = 9.81 # m/s^2
 mass = 10 #kg
 theta_degrees = 0
 theta_rad = math.radians(theta_degrees)
-
-# Matplotlib
-fig, ax = plt.subplots(figsize=(10,6))
+l = 100
 
 # Labels
-labels = {'W' : "Weight", 'C' : "Constraint"}
+labels = {'W' : "Weight", 'C' : "Constraint", 'P': "Parallel", 'R' : "Perpendicular", 'E' : "Equilibrium", 'S' : "Static Friction", 'K' : "Kinetic Friction"}
 
 def calculate_coords(l):
     x = l * math.cos(theta_rad)
@@ -28,32 +26,45 @@ def calculate_weight():
     f_perpendicular = f_weight * math.cos(theta_rad) #N = kg*m/s^2
     return f_weight, f_parallel, f_perpendicular
 
-def main():
+def draw_simulation():
+    # Matplotlib subplot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.subplots_adjust(left=0.3)
 
-    l = 100
     x, y = calculate_coords(l)
-    x0, y0 = x/2, y/2
+    x0, y0 = x / 2, y / 2
     f_weight, f_parallel, f_perpendicular = calculate_weight()
-    draw_simulation(x, y, x0, y0, f_weight, f_perpendicular)
 
-def draw_simulation(x, y, x0, y0, f_weight, f_perpendicular):
     ax.plot([0, x], [0, 0], 'gray', linestyle='--')  # Base
     ax.plot([x, x], [0, y], 'gray', linestyle='--')  # Altezza
     ax.plot([0, x], [0, y], 'black', linewidth=3)  # Piano effettivo
-    ax.quiver(x0, y0, 0, -f_weight, color='red', label=labels['W'], angles='xy', scale_units='xy', scale=10)
+    vec_w = ax.quiver(x0, y0, 0, -f_weight, color='red', label=labels['W'], angles='xy', scale_units='xy', scale=10)
     # x & y are the coordinates of the origin point, u & v are the components of the vector
     un = -f_perpendicular * math.sin(theta_rad)
     vn = f_perpendicular * math.cos(theta_rad)
-    ax.quiver(x0, y0, un, vn, color='blue', label=labels['C'], angles='xy', scale_units='xy', scale=10)
-
+    vec_c = ax.quiver(x0, y0, un, vn, color='blue', label=labels['C'], angles='xy', scale_units='xy', scale=10)
     ax.axis('equal')
     ax.legend()
     ax.grid(True)
-    fig.subplots_adjust(left=0.3)
-    ax_buttons = fig.add_axes([0.05, 0.4, 0.15, 0.25])
-    fig.show()
+    ax_buttons_values = fig.add_axes([0.05, 0.4, 0.15, 0.25])
+    ax_buttons_name = [labels['W'], labels['C']]
+    ax_buttons_init_state = [True, True]
+    check = CheckButtons(ax_buttons_values, ax_buttons_name, ax_buttons_init_state)
+
+    def toggle_vectors(label):
+        if label == labels['W']:
+            vec_w.set_visible(not vec_w.get_visible())
+        elif label == labels['C']:
+            vec_c.set_visible(not vec_c.get_visible())
+        fig.canvas.draw_idle()
+
+    check.on_clicked(toggle_vectors)
+    return check
 
 
+def main():
+    plt_sim = draw_simulation()
+    plt.show()
 
 if __name__ == '__main__':
     main()
